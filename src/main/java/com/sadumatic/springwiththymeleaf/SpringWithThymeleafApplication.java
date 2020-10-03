200721651e6d011e8b6ad42708a423321331ba2b
thierno sadou diallo <thierved01@gmail.com>
2020-10-03 16:08:27 +0000

    initiated the project

---
+++ src/main/java/com/sadumatic/springwiththymeleaf/SpringWithThymeleafApplication.java
@@ -0,0 +1,13 @@
+package com.sadumatic.springwiththymeleaf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringWithThymeleafApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringWithThymeleafApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sadumatic/springwiththymeleaf/SpringWithThymeleafApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sadumatic.springwiththymeleaf;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringWithThymeleafApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
